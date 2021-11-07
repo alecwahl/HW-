c89cd3e09ca9afef6d21625e4392a21f95ed638f
alecwahl <54637851+alecwahl@users.noreply.github.com>
2021-11-07 17:16:18 -0500

    Add files via upload

--- filesystem.c
+++ filesystem.c
@@ -22,6 +22,7 @@ static struct alecfs_inode *alecfs_get_inode(struct super_block *sb, uint64_t in
 	struct alecfs_inode *afs_inode;
 	printk(KERN_ALERT "Looking up inode %ld on disk\n", inode_no);
 	bh = sb_bread(sb, ALECFS_INODE_BLOCK + inode_no); 
+	printk(KERN_ALERT "Found inode %ld on disk\n", inode_no);
 	afs_inode = (struct alecfs_inode *)bh->b_data;
 	return afs_inode;
 }
