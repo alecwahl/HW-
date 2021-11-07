f1906b847dfb10174da7132724eeaf87a793fe60
alecwahl <54637851+alecwahl@users.noreply.github.com>
2021-11-07 14:08:55 -0500

    Add files via upload

---
+++ mkfs.c
@@ -0,0 +1,40 @@
+#include <unistd.h>
+#include <stdio.h>
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <fcntl.h>
+#include <stdint.h>
+#include <stdlib.h>
+#include <string.h>
+
+struct alecfs_superblock {
+    uint64_t magic;
+	unsigned short data_block_map[2048];
+	unsigned short inode_block_map[2032];
+};
+
+struct alecfs_inode {
+	unsigned int inode_num;
+	unsigned int data_block_num;
+	unsigned int file_size;
+	unsigned int dir_child_count;
+	unsigned int type;
+};
+
+static int write_superblock()
+{
+	struct alecfs_superblock sb;
+	sb.magic = 101;
+	
+	fd = open("/dev/mmcblk0p3", O_RDWR);
+	ret = write(fd, &sb, sizeof(sb));
+
+	ret = write(fd, &sb, sizeof(sb));
+
+	printf("Super block written succesfully\n");
+	return 0;
+}
+
+int main(){
+	write_superblock();
+}
