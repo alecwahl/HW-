bee62a3176f373191cde3d08f3d8c7b1152d7cb1
alecwahl <54637851+alecwahl@users.noreply.github.com>
2021-11-07 19:06:11 -0500

    Add files via upload

--- filesystem.c
+++ filesystem.c
@@ -15,7 +15,6 @@ static struct alecfs_inode *alecfs_get_inode(struct super_block *sb, unsigned in
 	struct alecfs_inode *afs_inode;
 	printk(KERN_ALERT "Looking up inode %u on disk\n", ALECFS_INODE_BLOCK + inode_no);
 	bh = sb_bread(sb, ALECFS_INODE_BLOCK + inode_no); 
-	printk(KERN_ALERT "bh returned\n", bh);
 	afs_inode = (struct alecfs_inode *)bh->b_data;
 	printk(KERN_INFO "alecfs root inode_num [%u] data block num [%u] dir_child_count [%u] type [%u].\n", afs_inode->inode_num, afs_inode->inode_num, afs_inode->dir_child_count, afs_inode->type);
 	printk(KERN_ALERT "Found inode %ld on disk\n", inode_no);
@@ -65,6 +64,10 @@ static int alecfs_fill_super(struct super_block *sb, void *data, int silent){
         sb->s_op                = &alecfs_sops;
         sb->s_time_gran         = 1;
 
+		struct alecfs_inode *afs_inode;
+		bh = sb_bread(sb, 128);
+		afs_inode = (struct alecfs_inode *)bh->b_data;
+		printk(KERN_INFO "alecfs root inode_num [%u] data block num [%u] dir_child_count [%u] type [%u].\n", afs_inode->inode_num, afs_inode->inode_num, afs_inode->dir_child_count, afs_inode->type);
 		alecfs_get_inode(sb, 0);
 
 
