d1eb06ff8933584d2febf347675f2b745ccbe241
alecwahl <54637851+alecwahl@users.noreply.github.com>
2021-11-07 19:12:50 -0500

    Add files via upload

--- filesystem.c
+++ filesystem.c
@@ -63,7 +63,7 @@ static int alecfs_fill_super(struct super_block *sb, void *data, int silent){
         sb->s_time_gran         = 1;
 
 		struct alecfs_inode *afs_inode;
-		bh = sb_bread(sb, 128);
+		bh = sb_bread(sb, 64);
 		afs_inode = (struct alecfs_inode *)bh->b_data;
 		printk(KERN_INFO "alecfs root inode_num [%u] data block num [%u] dir_child_count [%u] type [%u].\n", afs_inode->inode_num, afs_inode->inode_num, afs_inode->dir_child_count, afs_inode->type);
 		alecfs_get_inode(sb, 0);
