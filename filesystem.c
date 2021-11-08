e10541ee8362b414f89d8233f815001c336e62c5
alecwahl <54637851+alecwahl@users.noreply.github.com>
2021-11-07 20:20:41 -0500

    Add files via upload

--- filesystem.c
+++ filesystem.c
@@ -55,14 +55,12 @@ static int alecfs_fill_super(struct super_block *sb, void *data, int silent){
 		
 		printk(KERN_INFO "The magic number obtained in disk is: [%llu]\n",sb_disk->magic);
 		printk(KERN_INFO "simplefs filesystem of version [%llu] formatted with a block size of [%llu] detected in the device.\n", sb_disk->version, sb_disk->block_size);
-		
+		sb_set_blocksize(sb, 512)
 		sb->s_magic 			= ALECFS_MAGIC;
 		sb->s_fs_info 			= sb_disk;
-		/*
         sb->s_blocksize         = 512;
 		sb->s_maxbytes          = 512;
 		sb->s_blocksize_bits	= 9;
-		*/
         sb->s_op                = &alecfs_sops;
 
 		struct alecfs_inode *afs_inode;
