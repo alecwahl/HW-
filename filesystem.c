4360ce49f37458b870e8c436df015597353a87a3
alecwahl <54637851+alecwahl@users.noreply.github.com>
2021-11-08 20:27:16 -0500

    Add files via upload

--- filesystem.c
+++ filesystem.c
@@ -119,9 +119,9 @@ static struct dentry *alecfs_lookup(struct inode *dir,struct dentry *dentry, uns
 		//printk(KERN_ALERT "DE %lu, DE->Inode %u\n",i,de_inode);
 		if (de_inode != zero) {
 			if(strcmp(name, de.file_name) == 0){
-				file_alecfs_inode =  alecfs_get_inode(sb, 1936287861 - de_inode);
+				file_alecfs_inode =  alecfs_get_inode(sb,de_inode);
 				file_inode = new_inode(sb);
-				file_inode->i_ino =  1936287861 - de_inode;
+				file_inode->i_ino =  de_inode;
 				inode_init_owner(file_inode, NULL, S_IFREG);
 				file_inode->i_sb = sb;
 				file_inode->i_op = &alecfs_file_inode_ops;
